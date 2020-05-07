68cb440f706ae24dd330c57d371bb57ebdbc09c1
kiyoesjh <hyeyaa125@gmail.com>
2020-05-07 18:02:36 +0900

    feat: routing setting #6

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Login from './components/Login';
+import TeamSelect from './components/TeamSelect';
+import Match from './components/Match';
 
 function App() {
-  return <div className="App">hello!</div>;
+  return (
+    <div>
+      <Router>
+        <Route path="/" exact={true} component={Login} />
+        <Route path="/team" component={TeamSelect} />
+        <Route path="/match" component={Match} />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
