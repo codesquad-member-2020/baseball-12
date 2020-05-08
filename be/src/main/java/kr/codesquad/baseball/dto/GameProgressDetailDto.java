ea5ab2edaa04a3a7225ec8964b8cf971ae0d3e2e
ragdoll <museopkim0214@gmail.com>
2020-05-09 02:03:08 +0900

    feat: 게임 진행 현황 DTO 설계 #9
    팀과 선수에 대한 각 VO를 필드 값으로 하는 DTO를 설계를 완료 하였습니다.
    - OffenseTeam Details
    - DefenseTeam Details
    - Away User
    - Home User
    - Status Board

---
+++ be/src/main/java/kr/codesquad/baseball/dto/GameProgressDetailDto.java
@@ -0,0 +1,23 @@
+package kr.codesquad.baseball.dto;
+
+import kr.codesquad.baseball.dto.teamVO.DefenseTeam;
+import kr.codesquad.baseball.dto.teamVO.OffenseTeam;
+import kr.codesquad.baseball.model.StatusBoard;
+import kr.codesquad.baseball.model.User;
+import lombok.*;
+
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+@Builder
+public class GameProgressDetailDto {
+
+    OffenseTeam offenseTeam;
+
+    DefenseTeam defenseTeam;
+
+    User awayuser;
+
+    User homeUser;
+
+    StatusBoard statusBoard;
+}
---
+++ be/src/main/java/kr/codesquad/baseball/model/StatusBoard.java
@@ -0,0 +1,14 @@
+package kr.codesquad.baseball.model;
+
+public class StatusBoard {
+
+    public static String judgement;
+
+    public static int inning;
+
+    public static int strike;
+
+    public static int ball;
+
+    public static int out;
+}
