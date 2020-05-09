390df8264c66da8a57c7875470488cbb66e5ec00
idevjinie <idevjinie@gmail.com>
2020-05-09 20:22:44 +0900

    feat: 중계 영역의 이닝 컬렉션 뷰 구성 #3

--- iOS/Baseball/Baseball/Controllers/PlayViewController.swift
+++ iOS/Baseball/Baseball/Controllers/PlayViewController.swift
@@ -9,9 +9,37 @@
 import UIKit
 
 class PlayViewController: UIViewController {
+    
+    @IBOutlet weak var inningCollectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        inningCollectionView.dataSource = self
+        inningCollectionView.delegate = self
+        inningCollectionView.reloadData()
     }
 
+}
+
+extension PlayViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "inningCell", for: indexPath) as! InningCollectionViewCell
+        cell.inningLabel.text = "\(indexPath.item + 1)회"
+        return cell
+    }
+    
+}
+
+extension PlayViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 64, height: 50)
+    }
+    
 }
---
+++ iOS/Baseball/Baseball/Views/InningCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  InningCollectionViewCell.swift
+//  Baseball
+//
+//  Created by jinie on 2020/05/09.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import UIKit
+
+class InningCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var inningLabel: UILabel!
+    
+}
