c0eace9fff07416be1a3046277ec0f82096d311e
ragdoll <museopkim0214@gmail.com>
2020-05-11 02:09:55 +0900

    feat: 한 Pitch 종료 시 팀 레코드 업데이트 기능 구현 #11
    - 한 회의 Pitch가 종료 시 팀 레코드 업데이트
    - 아웃 값에 따라 상태를 달리 저장하도록 구현

--- be/src/main/java/kr/codesquad/baseball/controller/GameController.java
+++ be/src/main/java/kr/codesquad/baseball/controller/GameController.java
@@ -2,8 +2,6 @@ package kr.codesquad.baseball.controller;
 
 import kr.codesquad.baseball.dto.GameInitializingRequestDto;
 import kr.codesquad.baseball.dto.GameProgressDetailDto;
-import kr.codesquad.baseball.dto.playerVO.Batter;
-import kr.codesquad.baseball.dto.playerVO.BatterDetail;
 import kr.codesquad.baseball.response.ApiResponse;
 import kr.codesquad.baseball.service.GameService;
 import org.springframework.http.HttpStatus;
@@ -11,9 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.Map;
 
 @RestController
 public class GameController {
@@ -28,5 +23,10 @@ public class GameController {
     public ResponseEntity<ApiResponse<GameProgressDetailDto>> start(@RequestBody GameInitializingRequestDto initializingRequestDto) {
         GameProgressDetailDto gameProgressDetailDto = gameService.initializeGame(initializingRequestDto);
         return new ResponseEntity(ApiResponse.OK(true, gameProgressDetailDto), HttpStatus.OK);
+    }
+
+    @PostMapping("/matches/pitch")
+    public ResponseEntity<ApiResponse<GameProgressDetailDto>> pitch(@RequestBody GameInitializingRequestDto initializingRequestDto) {
+        return new ResponseEntity(ApiResponse.OK(true, gameService.tryPitch(initializingRequestDto)));
     }
 }
--- be/src/main/java/kr/codesquad/baseball/service/TeamService.java
+++ be/src/main/java/kr/codesquad/baseball/service/TeamService.java
@@ -7,6 +7,8 @@ import kr.codesquad.baseball.dto.playerVO.Batter;
 import kr.codesquad.baseball.dto.playerVO.Pitcher;
 import kr.codesquad.baseball.dto.teamVO.DefenseTeam;
 import kr.codesquad.baseball.dto.teamVO.OffenseTeam;
+import kr.codesquad.baseball.model.Game;
+import kr.codesquad.baseball.model.StatusBoard;
 import kr.codesquad.baseball.model.TeamRecord;
 import org.springframework.stereotype.Service;
 
@@ -30,12 +32,13 @@ public class TeamService {
     }
 
     public OffenseTeam findOffenseTeamByIds(int gameId, int teamId, int inning) {
+        int currentBattingOrder = findCurrentBattingOrder(gameId, teamId, inning);
         TeamRecord offenseTeamRecord = teamDao.findTeamRecordByIds(gameId, teamId, inning);
         String teamName = teamDao.findTeamNameById(teamId);
         List<BatterSummary> onBases = playerService.findBasemanByPlayerIds(offenseTeamRecord.getFirstBaseman(),
                                                                            offenseTeamRecord.getSecondBaseman(),
                                                                            offenseTeamRecord.getThirdBaseman());
-        Batter batter = playerService.findBatterPlayerByTeamIdWithOrder(teamId, offenseTeamRecord.getCurrentBattingOrder());
+        Batter batter = playerService.findBatterPlayerByTeamIdWithOrder(teamId, currentBattingOrder);
         List<BatterDetail> battingRecords = playerService.findAllBattingRecordsOfCurrentInningByIds(gameId, teamId, offenseTeamRecord.getInning());
         return OffenseTeam.builder()
                           .teamId(teamId)
@@ -57,5 +60,24 @@ public class TeamService {
                           .score(defenseTeamRecord.getScore())
                           .pitcher(pitcher)
                           .build();
+    }
+
+    public int findCurrentBattingOrder(int gameId, int teamId, int inning) {
+        if (inning > 1) teamDao.findCurrentBattingOrderOfInning(gameId, teamId, inning - 1);
+        else teamDao.findCurrentBattingOrderOfInning(gameId, teamId, inning);
+    }
+
+    public void updateTeamRecordOfCurrentInning(StatusBoard statusBoard, Game game) {
+        teamDao.updateTeamRecordOfCurrentInning(statusBoard, game);
+    }
+
+    public void updateTeamRecordToChangeOffense(StatusBoard statusBoard, Game game) {
+        teamDao.updateTeamRecordOfCurrentInning(statusBoard, game);
+        teamDao.updateCurrentGameInformation(statusBoard.getInning(), !game.isFirsthalf(), game.getId());
+    }
+
+    public void updateTeamRecordToChangeInning(StatusBoard statusBoard, Game game) {
+        teamDao.updateTeamRecordOfCurrentInning(statusBoard, game);
+        teamDao.updateCurrentGameInformation(statusBoard.getInning() + 1, game.isFirsthalf(), game.getId());
     }
 }
