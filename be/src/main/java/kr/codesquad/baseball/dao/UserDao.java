19d42c4f4da942b41513dd7b29be4c7ebbfc3f92
ragdoll <museopkim0214@gmail.com>
2020-05-10 17:13:58 +0900

    feat: 홈팀 및 어웨이팀 유저 정보 가져오기 기능 구현 #12
    - away, home 유저의 정보를 가져오기 위하여 구현

--- be/src/main/java/kr/codesquad/baseball/dao/UserDao.java
+++ be/src/main/java/kr/codesquad/baseball/dao/UserDao.java
@@ -1,4 +1,38 @@
 package kr.codesquad.baseball.dao;
 
+import kr.codesquad.baseball.model.User;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+@Repository
 public class UserDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public UserDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public User findAwayUserByGameId(int gameId) {
+        String SQL = "SELECT u.id as userId, u.name as userName, u.email as userEmail " +
+                     "FROM user u INNER JOIN game g ON u.id = g.away_user " +
+                     "WHERE g.id = ?";
+        return DataAccessUtils.singleResult(jdbcTemplate.query(SQL, new Object[]{gameId},
+                (rs, rowNum) -> new User(rs.getInt("userId"),
+                                         rs.getString("userName"),
+                                         rs.getString("userEmail"))));
+    }
+
+    public User findHomeUserByGameId(int gameId) {
+        String SQL = "SELECT u.id as userId, u.name as userName, u.email as userEmail " +
+                     "FROM user u INNER JOIN game g ON u.id = g.home_user " +
+                     "WHERE g.id = ?";
+        return DataAccessUtils.singleResult(jdbcTemplate.query(SQL, new Object[]{gameId},
+                (rs, rowNum) -> new User(rs.getInt("userId"),
+                        rs.getString("userName"),
+                        rs.getString("userEmail"))));
+    }
 }
--- be/src/main/java/kr/codesquad/baseball/service/UserService.java
+++ be/src/main/java/kr/codesquad/baseball/service/UserService.java
@@ -1,4 +1,23 @@
 package kr.codesquad.baseball.service;
 
+import kr.codesquad.baseball.dao.UserDao;
+import kr.codesquad.baseball.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private final UserDao userDao;
+
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public User findAwayUserByGameId(int gameId) {
+        return userDao.findAwayUserByGameId(gameId);
+    }
+
+    public User findHomeUserByGameId(int gameId) {
+        return userDao.findHomeUserByGameId(gameId);
+    }
 }
