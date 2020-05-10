db0ff163f6ea9a8ac269dc856c56bdf635e7500d
ragdoll <museopkim0214@gmail.com>
2020-05-10 17:16:21 +0900

    fix: VO 클래스 이름 수정 (Batter -> BatterSummary) #12

--- be/src/main/java/kr/codesquad/baseball/dto/teamVO/OffenseTeam.java
+++ be/src/main/java/kr/codesquad/baseball/dto/teamVO/OffenseTeam.java
@@ -1,8 +1,8 @@
 package kr.codesquad.baseball.dto.teamVO;
 
+import kr.codesquad.baseball.dto.playerVO.BatterSummary;
 import kr.codesquad.baseball.dto.playerVO.BatterDetail;
 import kr.codesquad.baseball.dto.playerVO.Batter;
-import kr.codesquad.baseball.dto.playerVO.PlayerVO;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,14 +12,14 @@ import java.util.List;
 @Getter @Setter
 public class OffenseTeam extends TeamVO {
 
-    private List<PlayerVO> onBases;
+    private List<BatterSummary> onBases;
 
     private Batter batter;
 
     private List<BatterDetail> battingRecords;
 
     @Builder
-    public OffenseTeam(int teamId, String teamName, int score, List<PlayerVO> onBases, Batter batter, List<BatterDetail> battingRecords) {
+    public OffenseTeam(int teamId, String teamName, int score, List<BatterSummary> onBases, Batter batter, List<BatterDetail> battingRecords) {
         super(teamId, teamName, score);
         this.onBases = onBases;
         this.batter = batter;
