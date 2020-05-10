51838a0a6dbfdd0ed5cb91983a54629d7e699ac0
ragdoll <museopkim0214@gmail.com>
2020-05-10 17:31:10 +0900

    feat: 게임 경기 진행 현황에 대한 팀 정보 반환 기능 구현 #12
    공격팀과 수비팀에 따라 다음의 Player 매퍼 클래스를 갖는다.
    - OffenseTeam (Batter, BatterRecord, Baseman information)
    - DefenseTeam (Pitcher)

--- be/src/main/java/kr/codesquad/baseball/dao/TeamDao.java
+++ be/src/main/java/kr/codesquad/baseball/dao/TeamDao.java
@@ -1,5 +1,6 @@
 package kr.codesquad.baseball.dao;
 
+import kr.codesquad.baseball.model.TeamRecord;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -21,12 +22,34 @@ public class TeamDao {
     }
 
     public void insertTeamRecordOfInning(int gameId, int teamId, int inning) {
-        String sql = "INSERT INTO team_record (game, team, inning) " +
+        String SQL = "INSERT INTO team_record (game, team, inning) " +
                      "VALUES (:game, :team, :inning)";
         SqlParameterSource namedParameters = new MapSqlParameterSource()
                                                 .addValue("game", gameId)
                                                 .addValue("team", teamId)
                                                 .addValue("inning", inning);
-        namedParameterJdbcTemplate.update(sql, namedParameters);
+        namedParameterJdbcTemplate.update(SQL, namedParameters);
+    }
+
+    public String findTeamNameById(int teamId) {
+        String SQL = "SELECT name FROM team WHERE id = ?";
+        return jdbcTemplate.queryForObject(SQL, new Object[]{teamId}, String.class);
+    }
+
+    public TeamRecord findTeamRecordByIds(int gameId, int teamId, int inning) {
+        String SQL = "SELECT id, game, team, inning, score, current_batting_order, first_baseman, second_baseman, third_baseman " +
+                     "FROM team_record WHERE game = ? AND team = ? AND inning = ?";
+        return jdbcTemplate.queryForObject(SQL, new Object[]{gameId, teamId, inning},
+                (rs, rowNum) -> TeamRecord.builder()
+                        .id(rs.getInt("id"))
+                        .gameId(rs.getInt("game"))
+                        .teamId(rs.getInt("team"))
+                        .inning(rs.getInt("inning"))
+                        .score(rs.getInt("score"))
+                        .currentBattingOrder(rs.getInt("current_batting_order"))
+                        .firstBaseman(rs.getInt("first_baseman"))
+                        .secondBaseman(rs.getInt("second_baseman"))
+                        .thirdBaseman(rs.getInt("third_baseman"))
+                        .build());
     }
 }
--- be/src/main/java/kr/codesquad/baseball/service/TeamService.java
+++ be/src/main/java/kr/codesquad/baseball/service/TeamService.java
@@ -1,7 +1,13 @@
 package kr.codesquad.baseball.service;
 
 import kr.codesquad.baseball.dao.TeamDao;
-import kr.codesquad.baseball.model.Game;
+import kr.codesquad.baseball.dto.playerVO.BatterDetail;
+import kr.codesquad.baseball.dto.playerVO.BatterSummary;
+import kr.codesquad.baseball.dto.playerVO.Batter;
+import kr.codesquad.baseball.dto.playerVO.Pitcher;
+import kr.codesquad.baseball.dto.teamVO.DefenseTeam;
+import kr.codesquad.baseball.dto.teamVO.OffenseTeam;
+import kr.codesquad.baseball.model.TeamRecord;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,14 +17,45 @@ public class TeamService {
 
     private final TeamDao teamDao;
 
-    public TeamService(TeamDao teamDao) {
+    private final PlayerService playerService;
+
+    public TeamService(TeamDao teamDao, PlayerService playerService) {
         this.teamDao = teamDao;
+        this.playerService = playerService;
     }
 
     public void initializeTeamRecordOfInning(int gameId, int awayTeamId, int homeTeamId, int nextInning, boolean isFirsthalf) {
-        if (isFirsthalf) teamDao.insertTeamRecordOfInning(gameId, awayTeamId, nextInning);
-        else teamDao.insertTeamRecordOfInning(gameId, homeTeamId, nextInning);
+        teamDao.insertTeamRecordOfInning(gameId, awayTeamId, nextInning);
+        teamDao.insertTeamRecordOfInning(gameId, homeTeamId, nextInning);
     }
 
+    public OffenseTeam findOffenseTeamByIds(int gameId, int teamId, int inning) {
+        TeamRecord offenseTeamRecord = teamDao.findTeamRecordByIds(gameId, teamId, inning);
+        String teamName = teamDao.findTeamNameById(teamId);
+        List<BatterSummary> onBases = playerService.findBasemanByPlayerIds(offenseTeamRecord.getFirstBaseman(),
+                                                                           offenseTeamRecord.getSecondBaseman(),
+                                                                           offenseTeamRecord.getThirdBaseman());
+        Batter batter = playerService.findBatterPlayerByTeamIdWithOrder(teamId, offenseTeamRecord.getCurrentBattingOrder());
+        List<BatterDetail> battingRecords = playerService.findAllBattingRecordsOfCurrentInningByIds(gameId, teamId, offenseTeamRecord.getInning());
+        return OffenseTeam.builder()
+                          .teamId(teamId)
+                          .teamName(teamName)
+                          .score(offenseTeamRecord.getScore())
+                          .onBases(onBases)
+                          .batter(batter)
+                          .battingRecords(battingRecords)
+                          .build();
+    }
 
+    public DefenseTeam findDefenseTeamByIds(int gameId, int teamId, int inning) {
+        TeamRecord defenseTeamRecord = teamDao.findTeamRecordByIds(gameId, teamId, inning);
+        String teamName = teamDao.findTeamNameById(teamId);
+        Pitcher pitcher = playerService.findPitcherByIds(gameId, teamId);
+        return DefenseTeam.builder()
+                          .teamId(teamId)
+                          .teamName(teamName)
+                          .score(defenseTeamRecord.getScore())
+                          .pitcher(pitcher)
+                          .build();
+    }
 }
