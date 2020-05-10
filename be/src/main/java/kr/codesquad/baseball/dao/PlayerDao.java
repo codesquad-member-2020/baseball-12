0a83b2131c4eb27b7008170c904cbc41d8671f26
ragdoll <museopkim0214@gmail.com>
2020-05-10 17:28:33 +0900

    feat: 게임 진행 상태를 각 플레이어 매핑 클래스에 반환 하는 기능 구현 #12
    - Pitcher
    - Batter
    - Baseman
    - All player record

--- be/src/main/java/kr/codesquad/baseball/dao/PlayerDao.java
+++ be/src/main/java/kr/codesquad/baseball/dao/PlayerDao.java
@@ -1,5 +1,11 @@
 package kr.codesquad.baseball.dao;
 
+import kr.codesquad.baseball.dto.playerVO.BatterSummary;
+import kr.codesquad.baseball.dto.playerVO.Batter;
+import kr.codesquad.baseball.dto.playerVO.Pitcher;
+import kr.codesquad.baseball.model.BattingRecord;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -22,16 +28,78 @@ public class PlayerDao {
     }
 
     public List<Integer> findPlayerIdsByTeamId(int teamId) {
-        String sql = "SELECT id FROM player WHERE team = ?";
-        return jdbcTemplate.queryForList(sql, new Object[]{teamId}, Integer.class);
+        String SQL = "SELECT id FROM player WHERE team = ?";
+        return jdbcTemplate.queryForList(SQL, new Object[]{teamId}, Integer.class);
+    }
+
+    public List<Integer> findRecordedPlayerIds(int gameId, int teamId, int currentInning) {
+        String SQL = "SELECT p.id FROM player p INNER JOIN batting_record br ON p.id = br.player " +
+                     "INNER JOIN player_record pr ON p.id = pr.player " +
+                     "WHERE br.game = ? AND p.team = ? AND br.inning = ? AND pr.plate_appearance > 0";
+        return jdbcTemplate.queryForList(SQL, new Object[]{gameId, teamId, currentInning}, Integer.class);
     }
 
     public void insertPlayerRecord(int gameId, int playerId) {
-        String sql = "INSERT INTO player_record (game, player) " +
+        String SQL = "INSERT INTO player_record (game, player) " +
                      "VALUES (:game, :player)";
         SqlParameterSource namedParameters = new MapSqlParameterSource()
                                             .addValue("game", gameId)
                                             .addValue("player", playerId);
-        namedParameterJdbcTemplate.update(sql, namedParameters);
+        namedParameterJdbcTemplate.update(SQL, namedParameters);
+    }
+
+    public BatterSummary findBatterSummaryById(int playerId) throws EmptyResultDataAccessException {
+        String SQL = "SELECT id, name, batting_order FROM player WHERE id = ?";
+        return DataAccessUtils.singleResult(jdbcTemplate.query(SQL, new Object[]{playerId},
+                (rs, rowNum) -> BatterSummary.builder()
+                                             .playerId(rs.getInt("id"))
+                                             .playerName(rs.getString("name"))
+                                             .battingOrder(rs.getInt("batting_order"))
+                                             .build()));
+    }
+
+    public Batter findBatterPlayerByTeamIdWithOrder(int awayTeamId, int currentBattingOrder) {
+        String SQL = "SELECT p.id as playerId, p.name as playerName, p.batting_order as battingOrder, " +
+                            "pr.plate_appearance as plateAppearance, pr.hit_count as hitCount " +
+                     "FROM player p LEFT OUTER JOIN player_record pr ON p.id = pr.player " +
+                     "WHERE p.team = ? AND p.batting_order = ?";
+        return jdbcTemplate.queryForObject(SQL, new Object[]{awayTeamId, currentBattingOrder},
+                (rs, rowNum) -> Batter.batterBuilder()
+                                      .playerId(rs.getInt("playerId"))
+                                      .playerName(rs.getString("playerName"))
+                                      .battingOrder(rs.getInt("battingOrder"))
+                                      .plateAppearance(rs.getInt("plateAppearance"))
+                                      .hitCount(rs.getInt("hitCount"))
+                                      .build());
+    }
+
+    public List<BattingRecord> findBattingRecordsOfInningByIds(int gameId, int playerId, int currentInning) {
+        String SQL = "SELECT br.id as id, br.game as gameId, br.player as playerId, br.inning as inning, br.judgement as judgement, " +
+                            "br.strike_count as strikeCount, br.ball_count as ballCount, br.out_count as outCount " +
+                     "FROM batting_record br INNER JOIN player p ON br.player = p.id " +
+                     "WHERE br.game = ? AND br.player = ? AND br.inning = ?";
+        return jdbcTemplate.query(SQL, new Object[]{gameId, playerId, currentInning},
+                (rs, rowNum) -> BattingRecord.builder()
+                                             .id(rs.getInt("id"))
+                                             .game(rs.getInt("gameId"))
+                                             .player(rs.getInt("playerId"))
+                                             .inning(rs.getInt("inning"))
+                                             .judgement(rs.getString("judgement"))
+                                             .strike(rs.getInt("strikeCount"))
+                                             .ball(rs.getInt("ballCount"))
+                                             .out(rs.getInt("outCount"))
+                                             .build());
+    }
+
+    public Pitcher findPitcherByIds(int gameId, int teamId) {
+        String SQL = "SELECT p.id as playerId, p.name as playerName, pr.pitching_count as pitchingCount " +
+                     "FROM player p INNER JOIN player_record pr ON p.id = pr.player " +
+                     "WHERE pr.game = ? AND p.team = ? AND p.position = 'pitcher'";
+        return DataAccessUtils.singleResult(jdbcTemplate.query(SQL, new Object[]{gameId, teamId},
+                (rs, rowNum) -> Pitcher.builder()
+                                       .playerId(rs.getInt("playerId"))
+                                       .playerName(rs.getString("playerName"))
+                                       .pitchingCount(rs.getInt("pitchingCount"))
+                                       .build()));
     }
 }
--- be/src/main/java/kr/codesquad/baseball/service/PlayerService.java
+++ be/src/main/java/kr/codesquad/baseball/service/PlayerService.java
@@ -1,9 +1,16 @@
 package kr.codesquad.baseball.service;
 
 import kr.codesquad.baseball.dao.PlayerDao;
+import kr.codesquad.baseball.dto.playerVO.BatterDetail;
+import kr.codesquad.baseball.dto.playerVO.BatterSummary;
+import kr.codesquad.baseball.dto.playerVO.Batter;
+import kr.codesquad.baseball.dto.playerVO.Pitcher;
+import kr.codesquad.baseball.model.BattingRecord;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PlayerService {
@@ -19,5 +26,35 @@ public class PlayerService {
         List<Integer> homePlayerIds = playerDao.findPlayerIdsByTeamId(homeTeamId);
         awayPlayerIds.forEach(awayId -> playerDao.insertPlayerRecord(gameId, awayId));
         homePlayerIds.forEach(homeId -> playerDao.insertPlayerRecord(gameId, homeId));
+    }
+
+    public List<BatterSummary> findBasemanByPlayerIds(Integer firstBaseman, Integer secondBaseman, Integer thirdBaseman) {
+        List<Integer> basemanIds = Arrays.asList(firstBaseman, secondBaseman, thirdBaseman);
+        return basemanIds.stream().map(playerDao::findBatterSummaryById).collect(Collectors.toList());
+    }
+
+    public Batter findBatterPlayerByTeamIdWithOrder(int awayTeamId, int currentBattingOrder) {
+        return playerDao.findBatterPlayerByTeamIdWithOrder(awayTeamId, currentBattingOrder);
+    }
+
+    public List<BatterDetail> findAllBattingRecordsOfCurrentInningByIds(int gameId, int teamId, int currentInning) {
+//        List<Integer> playerIds = playerDao.findPlayerIdsByTeamId(teamId);
+        List<Integer> recordedPlayerIds = playerDao.findRecordedPlayerIds(gameId, teamId, currentInning);
+        return recordedPlayerIds.stream()
+                .map((playerId) -> {
+                    BatterSummary batterSummary = playerDao.findBatterSummaryById(playerId);
+                    List<BattingRecord> battings = playerDao.findBattingRecordsOfInningByIds(gameId, playerId, currentInning);
+                    return BatterDetail.batterDetailBuilder()
+                                       .playerId(batterSummary.getPlayerId())
+                                       .playerName(batterSummary.getPlayerName())
+                                       .battingOrder(batterSummary.getBattingOrder())
+                                       .battings(battings)
+                                       .build();
+                })
+                .collect(Collectors.toList());
+    }
+
+    public Pitcher findPitcherByIds(int gameId, int teamId) {
+        return playerDao.findPitcherByIds(gameId, teamId);
     }
 }
