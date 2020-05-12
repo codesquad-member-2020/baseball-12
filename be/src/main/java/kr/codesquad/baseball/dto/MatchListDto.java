8d02cc542d777cde372b08b7400b31a9ac6433fb
ragdoll <museopkim0214@gmail.com>
2020-05-12 22:06:47 +0900

    feat: MatchListDto 추가 #10
    - 모든 매치 리스트를 가져오는 Dto 클래스 구현

---
+++ be/src/main/java/kr/codesquad/baseball/dto/MatchListDto.java
@@ -0,0 +1,20 @@
+package kr.codesquad.baseball.dto;
+
+import kr.codesquad.baseball.model.Team;
+import kr.codesquad.baseball.model.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Builder
+public class MatchListDto {
+
+    Team awayTeam;
+
+    Team homeTeam;
+
+    User awayUser;
+
+    User homeUser;
+}
