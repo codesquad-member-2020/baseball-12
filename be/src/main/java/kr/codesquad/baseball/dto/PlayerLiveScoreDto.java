8d1cb334233cb8ec7d4c3adb997cc95291d1c2d4
ragdoll <museopkim0214@gmail.com>
2020-05-13 19:57:55 +0900

    feat: 실시간 플레이어 성적 정보 요청 기능 구현을 위한 클래스 설계 #16
    - LiveScorePlayerVO
    - BatterLiveScoreVO
    - PlayerLiveScoreDto

--- be/src/main/java/kr/codesquad/baseball/dto/PlayerLiveScoreDto.java
+++ be/src/main/java/kr/codesquad/baseball/dto/PlayerLiveScoreDto.java
@@ -1,4 +1,20 @@
 package kr.codesquad.baseball.dto;
 
+import kr.codesquad.baseball.dto.teamVO.LiveScorePlayerVO;
+import kr.codesquad.baseball.model.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Builder
 public class PlayerLiveScoreDto {
+
+    LiveScorePlayerVO awayTeam;
+
+    LiveScorePlayerVO homeTeam;
+
+    User awayUser;
+
+    User homeUser;
 }
